dc206889852c3ba6099f1d93d0d0a2def8001348
Loic Fournerat <lfourner@student.42.fr>
2022-04-11 18:55:32 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+// Routes
+import Home from './routes/Home'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
