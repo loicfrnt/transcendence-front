ba1b47f9a60123b70ed0469aea2c0568d3ffc0b9
Loic Fournerat <lfourner@student.42.fr>
2022-04-13 11:50:25 +0200

    moved pong game to new component

---
+++ src/components/PongGame.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { GameCanvas } from '../pong/pong'
+
+export default function PongGame() {
+  useEffect(() => {
+    console.log('game loaded')
+    let canvas = document.querySelector(
+      'canvas#pongCanvas'
+    ) as HTMLCanvasElement
+    let game = new GameCanvas(canvas)
+    function listener(e: KeyboardEvent) {
+      game.playerMoveKB(e)
+    }
+
+    document.addEventListener('keydown', listener)
+    game.draw()
+    game.play()
+    return () => {
+      game.run = false
+      document.removeEventListener('keydown', listener)
+      console.log('game unloaded')
+    }
+  }, [])
+  return <canvas id="pongCanvas" width="640" height="480"></canvas>
+}
--- src/routes/Game/Game.tsx
+++ src/routes/Game/Game.tsx
@@ -1,31 +1,10 @@
 import { User } from '../../types/user'
 import { UserDesc } from '../../components/UserDesc'
-import { GameCanvas } from '../../pong/pong'
-import { useEffect } from 'react'
+import PongGame from '../../components/PongGame'
 
 function Game({ currUser }: { currUser: User }) {
   const instructions =
     'Use the arrow keys to move. Score 10 points to win the game'
-
-  useEffect(() => {
-    console.log('game loaded')
-    let canvas = document.querySelector(
-      'canvas#pongCanvas'
-    ) as HTMLCanvasElement
-    let game = new GameCanvas(canvas)
-    function listener(e: KeyboardEvent) {
-      game.playerMoveKB(e)
-    }
-
-    document.addEventListener('keydown', listener)
-    game.draw()
-    game.play()
-    return () => {
-      game.run = false
-      document.removeEventListener('keydown', listener)
-      console.log('game unloaded')
-    }
-  }, [])
 
   // useEffect(() => () => {
   //   document.removeEventListener('keydown', (e) => game.playerMoveKB(e))
@@ -37,13 +16,7 @@ function Game({ currUser }: { currUser: User }) {
         <h2>How to play</h2>
         <p>{instructions}</p>
       </div>
-      {/* TMP */}
-      <p>
-        Joueur 1 : <em id="player-score">0</em> - Joueur 2 :
-        <em id="oppenent-score">0</em>
-      </p>
-      {/* TMP */}
-      <canvas id="pongCanvas" width="640" height="480"></canvas>
+      <PongGame />
       <div className="oppenent">
         <div>Oppenent</div>
         <UserDesc user={currUser} />
