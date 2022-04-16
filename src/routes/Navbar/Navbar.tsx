5e587c597107f2ac32a0bc6f0bb64b94efa2713c
Loic Fournerat <lfourner@student.42.fr>
2022-04-16 14:43:34 +0200

    clean

--- src/routes/Navbar/Navbar.tsx
+++ src/routes/Navbar/Navbar.tsx
@@ -1,25 +1,10 @@
-import { Outlet, Link, NavLink } from 'react-router-dom'
+import { Outlet, NavLink } from 'react-router-dom'
 import { ReactComponent as Home } from './home.svg'
 import { ReactComponent as Game } from './game.svg'
-import { ReactComponent as GameActive } from './gameActive.svg'
 import { ReactComponent as Profile } from './profile.svg'
-import { ReactComponent as ProfileActive } from './profileActive.svg'
 import { ReactComponent as Chat } from './chat.svg'
-import { ReactComponent as ChatActive } from './chatActive.svg'
 
 function Navbar() {
-  // function renderLinkHome() {
-  //   return (
-  //     <NavLink to="" className="flex justify-center mb-10">
-  //       <div className="bg-gray-light h-[77px] w-[77px] rounded py-[4px]">
-  //         <div className="flex justify-center">
-  //           <img src={home} alt="Home" />
-  //         </div>
-  //       </div>
-  //     </NavLink>
-  //   )
-  // }
-
   function renderLink(
     to: string,
     Svg: React.FunctionComponent<
@@ -42,11 +27,6 @@ function Navbar() {
               }
             >
               <div className="flex justify-center">
-                {/* <img
-                  src={isActive ? svgActive : svg}
-                  alt={to}
-                  className="hover:fill-violet"
-                /> */}
                 <Svg
                   className={
                     isActive ? 'fill-violet' : 'fill-gray hover:fill-violet'
@@ -66,7 +46,6 @@ function Navbar() {
   return (
     <div className="flex h-full">
       <nav className="bg-white  space-y-6 flex flex-col w-[89px] px-[5px] pt-3 ">
-        {/* {renderLinkHome()} */}
         {renderLink('', Home)}
         {renderLink('game', Game)}
         {renderLink('profile', Profile)}
