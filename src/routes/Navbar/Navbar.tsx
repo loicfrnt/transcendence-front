1353f2b2c162f2f197dcd28c8337e5ffe3a441e2
Loic Fournerat <lfourner@student.42.fr>
2022-04-16 14:41:58 +0200

    Svg color now managed through css style attribute. allows to change color on hover without any js

--- src/routes/Navbar/Navbar.tsx
+++ src/routes/Navbar/Navbar.tsx
@@ -1,30 +1,37 @@
 import { Outlet, Link, NavLink } from 'react-router-dom'
-import home from './home.svg'
-import game from './game.svg'
-import gameActive from './gameActive.svg'
-import profile from './profile.svg'
-import profileActive from './profileActive.svg'
-import chat from './chat.svg'
-import chatActive from './chatActive.svg'
+import { ReactComponent as Home } from './home.svg'
+import { ReactComponent as Game } from './game.svg'
+import { ReactComponent as GameActive } from './gameActive.svg'
+import { ReactComponent as Profile } from './profile.svg'
+import { ReactComponent as ProfileActive } from './profileActive.svg'
+import { ReactComponent as Chat } from './chat.svg'
+import { ReactComponent as ChatActive } from './chatActive.svg'
 
 function Navbar() {
-  function renderLinkHome() {
-    return (
-      <NavLink to="" className="flex justify-center mb-10">
-        <div className="bg-gray-light h-[77px] w-[77px] rounded py-[4px]">
-          <div className="flex justify-center">
-            <img src={home} alt="Home" />
-          </div>
-        </div>
-      </NavLink>
-    )
-  }
+  // function renderLinkHome() {
+  //   return (
+  //     <NavLink to="" className="flex justify-center mb-10">
+  //       <div className="bg-gray-light h-[77px] w-[77px] rounded py-[4px]">
+  //         <div className="flex justify-center">
+  //           <img src={home} alt="Home" />
+  //         </div>
+  //       </div>
+  //     </NavLink>
+  //   )
+  // }
 
-  function renderLink(to: string, svg: string, svgActive: string) {
+  function renderLink(
+    to: string,
+    Svg: React.FunctionComponent<
+      React.SVGProps<SVGSVGElement> & {
+        title?: string | undefined
+      }
+    >
+  ) {
     return (
       <NavLink
         to={to}
-        className={'flex justify-center' + (svg === home ? ' mb-10' : '')}
+        className={'flex justify-center' + (Svg === Home ? ' mb-10' : '')}
       >
         {({ isActive }) => {
           return (
@@ -35,10 +42,15 @@ function Navbar() {
               }
             >
               <div className="flex justify-center">
-                <img
+                {/* <img
                   src={isActive ? svgActive : svg}
                   alt={to}
                   className="hover:fill-violet"
+                /> */}
+                <Svg
+                  className={
+                    isActive ? 'fill-violet' : 'fill-gray hover:fill-violet'
+                  }
                 />
               </div>
               <p className="text-center">
@@ -55,10 +67,10 @@ function Navbar() {
     <div className="flex h-full">
       <nav className="bg-white  space-y-6 flex flex-col w-[89px] px-[5px] pt-3 ">
         {/* {renderLinkHome()} */}
-        {renderLink('', home, home)}
-        {renderLink('game', game, gameActive)}
-        {renderLink('profile', profile, profileActive)}
-        {renderLink('chat', chat, chatActive)}
+        {renderLink('', Home)}
+        {renderLink('game', Game)}
+        {renderLink('profile', Profile)}
+        {renderLink('chat', Chat)}
       </nav>
       <Outlet />
     </div>
