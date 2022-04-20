c6a1ea21ac315d468c5a11984300e0f3ab26f42b
Loic Fournerat <lfourner@student.42.fr>
2022-04-20 22:07:01 +0200

    ^^ chapo chapo

--- src/routes/Chat/Chat.tsx
+++ src/routes/Chat/Chat.tsx
@@ -6,7 +6,7 @@ function Chat() {
     <MainContainer>
       <div className="flex items-center justify-center sm:justify-start w-full h-full flex-wrap">
         <ContentBox className="w-[400px]"></ContentBox>
-        <ContentBox className="w-[400px] sm:max-w-[836px]  sm:grow"></ContentBox>
+        <ContentBox className="w-[400px] sm:max-w-[836px] sm:grow"></ContentBox>
         <ContentBox className="w-[400px]"></ContentBox>
       </div>
     </MainContainer>
