ce189449e73f5cdb876613bd3686fa534bd0fd53
Mohamed Bouzaien <mbouzaie@student.42.fr>
2022-05-04 16:59:48 +0200

    qr code advancement

--- src/routes/Profile/MyProfile.tsx
+++ src/routes/Profile/MyProfile.tsx
@@ -10,9 +10,10 @@ import Friends from './Friends'
 import MatchHistory from './MatchHistory'
 import FriendRequests from './FriendRequests'
 import Blocked from './Blocked'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PopUpBox from '../../components/PopUpBox'
 import { ErrorMessage, Field, Form, Formik } from 'formik'
+import localFilesService from '../../services/local-files.service'
 
 interface Props {
   user: User
@@ -23,7 +24,9 @@ export default function UserProfile({ user, setConnected }: Props) {
   function logout() {
     setConnected(false)
   }
-  const [editOpen, setEditOpen] = useState(false)
+  const [editOpen, setEditOpen] = useState(false);
+  const [qrClassDiv, setQrClassDiv] = useState<string>('');
+  const [qrClicked, setQrClicked] = useState(false);
   const initialValues = {
 
   };
@@ -38,44 +41,63 @@ export default function UserProfile({ user, setConnected }: Props) {
 
   const checkboxEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
     alert(event.target.checked);
+    if (event.target.checked === true) {
+      setQrClassDiv('pointer-events-none opacity-20');
+      setQrClicked(true);
+    }
+
   }
+
+  const [img, setImg] = useState<string>();
+
+  useEffect( () =>{
+    const fetchImage = async() =>{
+      const imgUrl = await localFilesService.retriveFile(1);
+      setImg(imgUrl);
+    };
+    fetchImage();
+  }, []);
   return (
     <MainContainer>
       <PopUpBox open={editOpen} setOpen={setEditOpen}>
-        <Formik initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={handleUpdate}>
-        <Form className='w-full max-w-lg' >
+
+      {qrClicked && <img src={img} alt="your image name " />}
+        <div className={qrClassDiv}>
+          <Formik initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={handleUpdate}>
+          <Form className='w-full max-w-lg'>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+              <div className='w-full md:w-1/3 px-3 mb-6 mb:mb-0'>
+                <label htmlFor='username' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Username</label>
+                <Field className='appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet'name='username' type='text' placeholder='Mohamed' />
+              </div>
+              <div className='w-full md:w-2/3 px-3'>
+                <label htmlFor='email' className='block uppercase tracking-wide text-gray text-x font-blod mb-2'>Email</label>
+                <Field className='appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet'name='email' type='text' placeholder='Mohamed@bouz.mouk' />
+              </div>
+            </div>
+            <div className='flex flex-wrap -mx-3 mb-6'>
+              <div className='w-full md:w-1/2 px-3 mb-6 mb:mb-0'>
+                <label htmlFor='password' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Password</label>
+                <Field name="password" type="password" placeholder="Password" className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet"/>
+                <ErrorMessage name="password" component="div" className='text-red'/>
+              </div>
+              <div className='w-full md:w-1/2 px-3 mb-6 mb:mb-0'>
+                <label htmlFor='confirm_password' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Confirm password</label>
+                <Field name="confirm_password" type="password" placeholder="Confirm Password" className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet"/>
+                <ErrorMessage name="confirm_password" component="div" className='text-red'/>
+              </div>
+            </div>
+          </Form>
+          </Formik>
           <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full md:w-1/3 px-3 mb-6 mb:mb-0'>
-              <label htmlFor='username' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Username</label>
-              <Field className='appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet'name='username' type='text' placeholder='Mohamed' />
-            </div>
-            <div className='w-full md:w-2/3 px-3'>
-              <label htmlFor='email' className='block uppercase tracking-wide text-gray text-x font-blod mb-2'>Email</label>
-              <Field className='appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet'name='email' type='text' placeholder='Mohamed@bouz.mouk' />
-            </div>
+              <div className="form-check form-switch w-full md:w1/2 px-12 mb-6 mb:mb-0">
+              <label className="form-check-label inline-block text-gray-800"> Activate Two Factors Authentication
+                <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 mb-3 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm focus:accent-violet" type="checkbox" onChange={checkboxEvent}/>
+              </label>
+              </div>
           </div>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full md:w-1/2 px-3 mb-6 mb:mb-0'>
-              <label htmlFor='password' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Password</label>
-              <Field name="password" type="password" placeholder="Password" className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet"/>
-              <ErrorMessage name="password" component="div" className='text-red'/>
-            </div>
-            <div className='w-full md:w-1/2 px-3 mb-6 mb:mb-0'>
-              <label htmlFor='confirm_password' className='block uppercase tracking-wide text-gray-700 text-x font-blod mb-2'>Confirm password</label>
-              <Field name="confirm_password" type="password" placeholder="Confirm Password" className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-violet"/>
-              <ErrorMessage name="confirm_password" component="div" className='text-red'/>
-            </div>
-          </div>
-        </Form>
-        </Formik>
-        <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className="form-check form-switch w-full md:w1/2 px-12 mb-6 mb:mb-0">
-            <label className="form-check-label inline-block text-gray-800"> Activate Two Factors Authentication
-              <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 mb-3 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm focus:accent-violet" type="checkbox" onChange={checkboxEvent}/>
-            </label>
-            </div>
         </div>
       </PopUpBox>
       <ProfileMasonry>
