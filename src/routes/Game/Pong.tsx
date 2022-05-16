9030e3dcb6b29f2b063488cee87a0794f8ad6f69
Loic Fournerat <lfourner@student.42.fr>
2022-05-16 19:27:45 +0200

    basic results after game ends

--- src/routes/Game/Pong.tsx
+++ src/routes/Game/Pong.tsx
@@ -1,4 +1,4 @@
-import { User } from '../../types/user'
+import { RelStatus, User } from '../../types/user'
 import { useEffect, useRef, useState } from 'react'
 import MainContainer from '../../components/MainContainer'
 import FindMatch from './FindMatch'
@@ -8,6 +8,7 @@ import InQueue from './InQueue'
 import { io, Socket } from 'socket.io-client'
 import ConnectError from '../../components/ConnectError'
 import Game, { GameStatus } from '../../types/game'
+import Results from './Results'
 
 interface Props {
   currUser: User
@@ -33,8 +34,12 @@ export default function Pong({ currUser }: Props) {
     sockRef.current?.on('update', (game: Game) => {
       setGame(game)
     })
+    sockRef.current?.on('endGame', (game: Game) => {
+      setGame(game)
+    })
     return () => {
       sockRef.current?.off('update')
+      sockRef.current?.off('endGame')
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sockRef.current])
@@ -58,6 +63,17 @@ export default function Pong({ currUser }: Props) {
           game={game}
           setGame={setGame}
           currUser={currUser}
+        />
+      )
+    }
+    if (game?.status === GameStatus.ENDED) {
+      return (
+        <Results
+          currUser={currUser}
+          socket={socket}
+          game={game}
+          setGame={setGame}
+          setStep={setStep}
         />
       )
     } else {
---
+++ src/routes/Game/Results.tsx
@@ -0,0 +1,58 @@
+import { User } from '../../types/user'
+import PongGame from './PongGame'
+import UserDesc from './UserDesc'
+import ContentBox from '../../components/ContentBox'
+import { Socket } from 'socket.io-client'
+import Game from '../../types/game'
+import { setTokenSourceMapRange } from 'typescript'
+
+interface Props {
+  currUser: User
+  socket: Socket
+  game: Game
+  setGame: React.Dispatch<React.SetStateAction<Game | null>>
+  setStep: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Results({
+  currUser,
+  socket,
+  game,
+  setGame,
+  setStep,
+}: Props) {
+  const oppenent =
+    game.player1.user.id === currUser.id ? game.player2 : game.player1
+  const player =
+    game.player1.user.id === currUser.id ? game.player1 : game.player2
+  const winner = (oppenent.score ?? 1) < (player.score ?? 0)
+  const color = winner ? 'text-green' : 'text-red'
+  return (
+    <div
+      className={`flex flex-col items-center justify-center gap-10 h-full w-full`}
+    >
+      <ContentBox className="max-w-[255px] pt-[6px] flex flex-col items-center gap-2">
+        <h2
+          className={`font-body font-semibold text-[2rem] leading-[2.625rem] ${color}`}
+        >
+          {winner ? 'Victory' : 'Defeat'}
+        </h2>
+        <span className="font-semibold text-[2rem]">
+          {game.player1.score} - {game.player2.score}
+        </span>
+      </ContentBox>
+      <button
+        onClick={() => {
+          socket.emit('joinQueue', (rep: any) => console.log(rep))
+          setStep('queue')
+          setGame(null)
+        }}
+        className="bg-white p-3 rounded-2xl hover:bg-violet group ease-in-out duration-300 "
+      >
+        <p className="font-semibold text-[2rem] m-3 ease-in-out duration-300 group-hover:text-white">
+          Find another match
+        </p>
+      </button>
+    </div>
+  )
+}
