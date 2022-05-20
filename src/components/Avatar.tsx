6f71f2f46d470f17016ab5eba089189f0a686430
Loic Fournerat <lfourner@student.42.fr>
2022-05-20 15:26:57 +0200

    fixed Avatar states

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,8 +1,7 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import localFilesService from '../services/local-files.service'
 import usersService from '../services/users.service'
-import {ReactComponent as Add} from '../assets/add.svg'
-import { stat } from 'fs'
+import { ReactComponent as Add } from '../assets/add.svg'
 import { UserStatus } from '../types/user'
 import { Link } from 'react-router-dom'
 
@@ -26,86 +25,84 @@ export default function Avatar({
   addStatus = false,
 }: AvatarProps) {
   const classes = `block bg-cover rounded-full aspect-square ${size}`
-  const [state, setState] = useState({
-    username : username,
-    avatarId : avatarId,
-    status: status
-  });
-  const [img, setImg] = useState<string>();
+  // const [state, setState] = useState({
+  //   username : username,
+  //   avatarId : avatarId,
+  //   status: status
+  // });
+  const [img, setImg] = useState<string>()
   const [badge, setBadge] = useState({
     color: 'bg-gray-400',
-    title: 'unavailable'
-  });
-  useEffect(() =>{
-    setState({
-      username : username,
-      avatarId : avatarId,
-      status: status
-    });
-  }, [status]);
-  useEffect( () =>{
-    const fetchImage = async() =>{
-      const imgUrl = await localFilesService.retriveFile(state.avatarId);
-      setImg(imgUrl);
-    };
-    fetchImage();
-  }, [state.avatarId]);
+    title: 'unavailable',
+  })
+  // useEffect(() =>{
+  //   setState({
+  //     username : username,
+  //     avatarId : avatarId,
+  //     status: status
+  //   });
+  // }, [status]);
   useEffect(() => {
-    if (state.status === UserStatus.Online)
-    {
+    const fetchImage = async () => {
+      const imgUrl = await localFilesService.retriveFile(avatarId)
+      setImg(imgUrl)
+    }
+    fetchImage()
+  }, [avatarId])
+  useEffect(() => {
+    if (status === UserStatus.Online) {
       setBadge({
         color: 'bg-green',
-        title: 'connected'
-      });
-    }
-    else if (state.status === UserStatus.Playing)
-    {
+        title: 'connected',
+      })
+    } else if (status === UserStatus.Playing) {
       setBadge({
         color: 'bg-orange',
-        title: 'Playing'
-      });
-    }
-    else
-    {
+        title: 'Playing',
+      })
+    } else {
       setBadge({
         color: 'bg-gray-400',
-        title: 'unavailable'
-      });
+        title: 'unavailable',
+      })
     }
-  }, [state.status])
-  const imageUploader = useRef(document.createElement("input"));
+  }, [status])
+  const imageUploader = useRef(document.createElement('input'))
   const handleSetImage = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.files)
-      return;
-    else
-    {
-      console.log(event.target.files);
-      let fd = new FormData();
-      fd.append('file', event.target.files[0]);
+    if (!event.target.files) return
+    else {
+      console.log(event.target.files)
+      let fd = new FormData()
+      fd.append('file', event.target.files[0])
       usersService.uploadFile(fd).then((response) => {
-        localStorage.setItem("user", JSON.stringify(response.data));
-        setState({
-          avatarId: response.data.avatar_id,
-          username: response.data.username,
-          status: response.data.status
-        });
-      });
+        localStorage.setItem('user', JSON.stringify(response.data))
+        // setState({
+        //   avatarId: response.data.avatar_id,
+        //   username: response.data.username,
+        //   status: response.data.status,
+        // })
+      })
     }
   }
   if (!withStatus) {
-    if (!noLink)
-    {
+    if (!noLink) {
       return (
         <div>
-        <Link to={'/profile/' + username}><img src={img}  className={classes} alt='Profile pic'/> </Link>
+          <Link to={'/profile/' + username}>
+            <img src={img} className={classes} alt="Profile pic" />{' '}
+          </Link>
         </div>
       )
-    }
-    else
+    } else
       return (
-        <div className='flex relative'>
-          <img src={img}  className={classes} alt='Profile pic'/>
-          {addStatus && <span className={`inline-flex items-center p-3 mr-1 text-sm font-semibold ${badge.color} rounded-full absolute`} title={badge.title}></span>}
+        <div className="flex relative">
+          <img src={img} className={classes} alt="Profile pic" />
+          {addStatus && (
+            <span
+              className={`inline-flex items-center p-3 mr-1 text-sm font-semibold ${badge.color} rounded-full absolute`}
+              title={badge.title}
+            ></span>
+          )}
         </div>
       )
   }
@@ -113,16 +110,22 @@ export default function Avatar({
   return (
     <div>
       <input
-          type="file"
-          accept="image/*"
-          onChange={e => handleSetImage(e)}
-          ref={imageUploader}
-          style={{
-            display: "none"
-          }}
-        />
-      <Add title='Upload Profile Image' className='ease-in-out duration-300 fill-gray group-hover:fill-violet float-right' onClick={() => {imageUploader.current.click()}}/>
-      <img src={img} className={classes} alt='Profile pic'/>
+        type="file"
+        accept="image/*"
+        onChange={(e) => handleSetImage(e)}
+        ref={imageUploader}
+        style={{
+          display: 'none',
+        }}
+      />
+      <Add
+        title="Upload Profile Image"
+        className="ease-in-out duration-300 fill-gray group-hover:fill-violet float-right"
+        onClick={() => {
+          imageUploader.current.click()
+        }}
+      />
+      <img src={img} className={classes} alt="Profile pic" />
     </div>
   )
 }
