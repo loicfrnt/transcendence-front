39fa6d4713130aaa0acb9116845bd4b7769b8ac8
Loic Fournerat <lfourner@student.42.fr>
2022-05-20 19:56:23 +0200

    changed uplopad profile picture button a bit

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import localFilesService from '../services/local-files.service'
 import usersService from '../services/users.service'
-import { ReactComponent as Add } from '../assets/add.svg'
+import { ReactComponent as Edit } from '../assets/pen.svg'
 import { User, UserStatus } from '../types/user'
 import { Link } from 'react-router-dom'
 
@@ -93,7 +93,7 @@ export default function Avatar({
   }
 
   return (
-    <div>
+    <div className="relative">
       <input
         type="file"
         accept="image/*"
@@ -103,13 +103,15 @@ export default function Avatar({
           display: 'none',
         }}
       />
-      <Add
-        title="Upload Profile Image"
-        className="ease-in-out duration-300 fill-gray group-hover:fill-violet float-right"
+      <button
+        title="Upload Profile Picture"
+        className="absolute top-[8%] right-[8%] duration-300 border border-gray bg-white hover:bg-gray h-14 w-14 rounded-full flex items-center justify-center group"
         onClick={() => {
           imageUploader.current.click()
         }}
-      />
+      >
+        <Edit className="h-8 w-8 ease-in-out duration-300 fill-gray group-hover:fill-violet float-right" />
+      </button>
       <img src={img} className={classes} alt="Profile pic" />
     </div>
   )
