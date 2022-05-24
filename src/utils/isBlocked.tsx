8f45c0b031ad11105ef6f25c6e1eb7307e2ac5ed
Loic Fournerat <lfourner@student.42.fr>
2022-05-24 17:20:31 +0200

    oups

--- src/utils/isBlocked.tsx
+++ src/utils/isBlocked.tsx
@@ -1,6 +1,6 @@
 import { ProtoUser, RelStatus, User } from '../types/user'
 
-export default function isFriend(currUser: User, secondUser: ProtoUser) {
+export default function isBlocked(currUser: User, secondUser: ProtoUser) {
   if (currUser.sent_relationships) {
     for (let relationship of currUser.sent_relationships) {
       if (
